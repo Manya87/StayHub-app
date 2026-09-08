068b76b92e3d5e4d97b342d41d6bd5e542d7277c
manya87 <manyagowda48@gmail.com>
2026-09-08 21:18:38 +0530

    created the project structure

---
+++ StayHub-app/backend/src/main/java/com/stayHub/StayHub_app/StayHubAppApplication.java
@@ -0,0 +1,13 @@
+package com.stayHub.StayHub_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHubAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayHubAppApplication.class, args);
+	}
+
+}
---
+++ StayHub-app/backend/src/test/java/com/stayHub/StayHub_app/StayHubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayHub.StayHub_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayHubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
